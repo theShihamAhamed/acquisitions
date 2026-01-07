a2dfe9799963dfada0b89885aa4d0e42cf6e28c2
shiham ahamed <it23690516@my.sliit.lk>
2026-01-07 12:01:07 +0530

    Implement Eslint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Aquisitions!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Aquisitions!');
 });
 
 export default app;
