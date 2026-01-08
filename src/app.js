398e872405fc439bb77fdd4a860fb1826bcc6b99
shiham ahamed <it23690516@my.sliit.lk>
2026-01-08 14:07:12 +0530

    impliment Arcjet security

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auht.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from acquisition!');
